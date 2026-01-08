85fca5520ca57e02bff0182ca847687cd02de973
SusiBaril <rosaleskeegan@gmail.com>
2026-01-08 10:33:34 +0800

    About, Skills and Contact

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const copyBtn = document.getElementById('copy-number');
+const toast = document.getElementById('toast');
+
+copyBtn.addEventListener('click', function(e) {
+
+    e.preventDefault();
+    const number = "+639686689357";
+
+    navigator.clipboard.writeText(number).then(function() {
+
+        toast.classList.add('show');
+        setTimeout(function() {
+            toast.classList.remove('show');
+        }, 3000);
+
+    }).catch(function(err) {
+        console.error('Could not copy text: ', err);
+    });
+
+});
